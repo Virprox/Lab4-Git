3b9b0f305d4befec3884d4410c9b8485b7c189c0
Virprox <Piotrek.szmigielski123@wp.pl>
2025-05-08 14:14:16 +0200

    Dodano flagi -h, -l, -d oraz zaktualizowano --help

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
     date
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ] && [ -z "$2" ]; then
+    for i in {1..100}
+    do
+        echo "Nazwa pliku: log$i.txt, Skrypt: $0, Data: $(date)" > log$i.txt
+    done
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ] && [ -n "$2" ]; then
+    for i in $(seq 1 $2)
+    do
+        echo "Nazwa pliku: log$i.txt, Skrypt: $0, Data: $(date)" > log$i.txt
+    done
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    echo "Dostępne opcje:"
+    echo "--date, -d : Wyświetla dzisiejszą datę"
+    echo "--logs, -l : Tworzy 100 plików logx.txt"
+    echo "--logs <liczba>, -l <liczba> : Tworzy podaną liczbę plików logx.txt"
+    echo "--help, -h : Wyświetla pomoc"
 fi
